dd59e418b86962cb3285be0caf2ef5014b841d39
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-29 12:51:39 -0700

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,18 @@
-// Code DelayedButton Component Here
+import ReactSixteenAdapter from 'enzyme-adapter-react-16';
+import React from 'react';
+
+class DelayedButton extends React.Component {
+
+  handleClick = (event) => {
+    event.persist();
+    window.setTimeout(() => {this.props.onDelayedClick(event)}, this.props.delay)
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick} >Click this button!</button>
+    )
+  }
+}
+
+export default DelayedButton
